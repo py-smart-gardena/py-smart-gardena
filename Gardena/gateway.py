3044a5a0c437d15060e4fa44e9fa6eda7ec3d819
Grm <grm.klein@gmail.com>
2018-12-30 01:31:08 +0100

    add authenticate and create header function

--- Gardena/gateway.py
+++ Gardena/gateway.py
@@ -5,9 +5,29 @@ class Gateway:
     """Class to communicate with smart gateway and handle network calls"""
 
     def __init__(self, email=None, password=None, debug=False):
+        """Constructor, create instance of gateway"""
         if email is None or password is None:
             raise ValueError("Arguments 'email' and 'passwords' are required")
         self.debug = debug
         self.email = email
         self.password = password
+        self.token = None
+        self.etag = None
         self.request_session = requests.session()
+
+    def authenticate(self):
+        """Authenticate and get tokens """
+        credentials = {
+            "email": self.email,
+            "password": self.password
+        }
+
+    def __create_header(self):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        if self.token is not None:
+            headers['X-Session'] = self.token
+        if self.etag is not None:
+            headers['If-None-Match'] = ETag
+        return headers
