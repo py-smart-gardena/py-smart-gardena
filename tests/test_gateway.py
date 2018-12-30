5792154eb13d14ec5b1809df64532ea6abef1efb
Grm <grm.klein@gmail.com>
2018-12-30 16:16:50 +0100

    Add test for email and password missing in gateway

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -1,7 +1,18 @@
 from gardena.gateway import Gateway
+import pytest
 
 
 def test_init():
     gw = Gateway(email='test@test.com', password='password')
     assert gw.email == 'test@test.com'
     assert gw.password == 'password'
+
+
+def test_init_exception_without_email():
+    with pytest.raises(ValueError):
+        gw = Gateway(password='password')
+
+
+def test_init_exception_without_password():
+    with pytest.raises(ValueError):
+        gw = Gateway(email='test@test.com')
