274838eabafd30d2dd056f89568312514a492b24
Grm <grm.klein@gmail.com>
2019-01-04 12:51:23 +0100

    Add common fields in BaseGardenaDeviceClass

--- src/gardena/base_gardena_class.py
+++ src/gardena/base_gardena_class.py
@@ -4,13 +4,6 @@ class BaseGardenaClass:
     id = None
     name = None
 
-    """Used to map data between 'device_info' ability fields and class fields"""
-    device_info_ability_fields = {
-        "serial_number": "serial_number",
-        "version": "version",
-        "last_time_online": "last_time_online",
-    }
-
     def __init__(self, smart_system=None):
         """Constructor, create instance of a gardena device"""
         if smart_system is None:
@@ -24,18 +17,3 @@ class BaseGardenaClass:
     def update_information(self, information):
         self.set_field_if_exists(information, "id", "id")
         self.set_field_if_exists(information, "name", "name")
-
-    def update_specific_device_info(self, device_specific_information):
-        pass
-
-    def handle_abilities(self, abilities):
-        for ability in abilities:
-            if ability["type"] == "device_info":
-                self.set_ability_field(ability, self.device_info_ability_fields)
-            else:
-                self.update_specific_device_info(ability)
-
-    def set_ability_field(self, hashmap, fields_hashmap):
-        for prop in hashmap["properties"]:
-            if prop["name"] in fields_hashmap:
-                setattr(self, fields_hashmap[prop["name"]], prop["value"])
--- src/gardena/base_gardena_device_class.py
+++ src/gardena/base_gardena_device_class.py
@@ -8,6 +8,28 @@ class BaseGardenaDeviceClass(BaseGardenaClass):
     category = None
     is_configuration_synchronized = False
 
+    """Used to map data between 'device_info' ability fields and class fields"""
+    device_info_ability_fields = {
+        "serial_number": "serial_number",
+        "version": "version",
+        "last_time_online": "last_time_online",
+    }
+
+    def handle_abilities(self, abilities):
+        for ability in abilities:
+            if ability["type"] == "device_info":
+                self.set_ability_field(ability, self.device_info_ability_fields)
+            else:
+                self.update_specific_device_info(ability)
+
+    def update_specific_device_info(self, device_specific_information):
+        pass
+
+    def set_ability_field(self, hashmap, fields_hashmap):
+        for prop in hashmap["properties"]:
+            if prop["name"] in fields_hashmap:
+                setattr(self, fields_hashmap[prop["name"]], prop["value"])
+
     def update_information(self, information):
         super(BaseGardenaDeviceClass, self).update_information(information)
         self.set_field_if_exists(information, "description", "description")
