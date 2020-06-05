368e920b590a902ff1cf4d6510bb1440079d37a6
Grm <grm.klein@gmail.com>
2020-06-05 21:54:58 +0200

    migrate to versioneer to manage versions

--- setup.py
+++ setup.py
@@ -3,36 +3,18 @@ from os.path import splitext, basename
 from glob import glob
 from subprocess import check_output
 
+import versioneer
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
-
-# Get git version (from https://github.com/pyfidelity/setuptools-git-version)
-command = "git describe --tags --long --dirty"
-fmt = "{tag}.{commitcount}+{gitsha}"
-
-
-def format_version(version, fmt=fmt):
-    parts = version.split("-")
-    assert len(parts) in (3, 4)
-    dirty = len(parts) == 4
-    tag, count, sha = parts[:3]
-    if count == "0" and not dirty:
-        return tag
-    return fmt.format(tag=tag, commitcount=count, gitsha=sha.lstrip("g"))
-
-
-def get_git_version():
-    git_version = check_output(command.split()).decode("utf-8").strip()
-    return format_version(version=git_version)
-
 
 tests_require = ["pytest>=3.6", "coverage", "pytest-cov", "requests_mock"]
 
 
 setuptools.setup(
     name="py-smart-gardena",
-    version=get_git_version(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="Jérémie Klein",
     author_email="grm.klein@gmail.com",
     description="This library aims to provide python way to communicate "
--- src/gardena/__init__.py
+++ src/gardena/__init__.py
@@ -4,3 +4,7 @@
 """
     Gardena gateway library
 """
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
