1bcbb9a8a16b457572da9514b4e90944b2667c7a
Jérémie Klein <jklein@octo.com>
2020-07-02 11:30:13 +0200

    Mower ids are now used to send commands

--- src/gardena/devices/mower.py
+++ src/gardena/devices/mower.py
@@ -12,6 +12,7 @@ class Mower(BaseDevice):
         self.operating_hours = "N/A"
         self.state = "N/A"
         self.last_error_code = "N/A"
+        self.mower_id = None
         self.setup_values_from_device_map(device_map)
 
     def update_device_specific_data(self, device_map):
@@ -21,35 +22,48 @@ class Mower(BaseDevice):
             self.set_attribute_value("operating_hours", device_map, "operatingHours")
             self.set_attribute_value("state", device_map, "state")
             self.set_attribute_value("last_error_code", device_map, "lastErrorCode")
+            self.set_attribute_value("mower_id", device_map, "id")
 
     def start_seconds_to_override(self, duration):
-        data = {
-            "id": str(uuid.uuid1()),
-            "type": "MOWER_CONTROL",
-            "attributes": {"command": "START_SECONDS_TO_OVERRIDE", "seconds": duration},
-        }
-        self.smart_system.call_smart_system_service(self.id, data)
+        if self.mower_id is not None:
+            data = {
+                "id": str(uuid.uuid1()),
+                "type": "MOWER_CONTROL",
+                "attributes": {"command": "START_SECONDS_TO_OVERRIDE", "seconds": duration},
+            }
+            self.smart_system.call_smart_system_service(self.id, data)
+        else:
+            self.smart_system.logger.error("The mower id is not defined")
 
     def start_dont_override(self, duration):
-        data = {
-            "id": str(uuid.uuid1()),
-            "type": "MOWER_CONTROL",
-            "attributes": {"command": "START_DONT_OVERRIDE", "seconds": duration},
-        }
-        self.smart_system.call_smart_system_service(self.id, data)
+        if self.mower_id is not None:
+            data = {
+                "id": str(uuid.uuid1()),
+                "type": "MOWER_CONTROL",
+                "attributes": {"command": "START_DONT_OVERRIDE", "seconds": duration},
+            }
+            self.smart_system.call_smart_system_service(self.id, data)
+        else:
+            self.smart_system.logger.error("The mower id is not defined")
 
     def park_until_next_task(self):
-        data = {
-            "id": str(uuid.uuid1()),
-            "type": "MOWER_CONTROL",
-            "attributes": {"command": "PARK_UNTIL_NEXT_TASK"},
-        }
-        self.smart_system.call_smart_system_service(self.id, data)
+        if self.mower_id is not None:
+            data = {
+                "id": str(uuid.uuid1()),
+                "type": "MOWER_CONTROL",
+                "attributes": {"command": "PARK_UNTIL_NEXT_TASK"},
+            }
+            self.smart_system.call_smart_system_service(self.id, data)
+        else:
+            self.smart_system.logger.error("The mower id is not defined")
 
     def park_until_further_notice(self):
-        data = {
-            "id": str(uuid.uuid1()),
-            "type": "MOWER_CONTROL",
-            "attributes": {"command": "PARK_UNTIL_FURTHER_NOTICE"},
-        }
-        self.smart_system.call_smart_system_service(self.id, data)
+        if self.mower_id is not None:
+            data = {
+                "id": str(uuid.uuid1()),
+                "type": "MOWER_CONTROL",
+                "attributes": {"command": "PARK_UNTIL_FURTHER_NOTICE"},
+            }
+            self.smart_system.call_smart_system_service(self.id, data)
+        else:
+            self.smart_system.logger.error("The mower id is not defined")
