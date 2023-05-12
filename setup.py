4dc9abe7daaa85991034c6fab8e6f407f009089a
Martin <pum.m@gmx.at>
2023-05-12 18:41:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     url="https://github.com/grm/py-smart-gardena",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["requests", "oauthlib", "requests_oauthlib", "websockets"],
+    install_requires=["requests", "websockets", "httpx", "authlib", "backoff"],
     setup_requires=["pytest-runner"],
     tests_require=tests_require,
     extras_require={"dev": ["pre-commit"]},
